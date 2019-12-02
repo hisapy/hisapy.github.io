b4e88ad469f321c10a0be5571b4b6958eca47802
hisa <hisa@iporaitech.com>
2019-12-02 14:20:17 -0300

    Add "For Hire!" to the top of the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+window.onload = () => {
+  const closeBtn = document.querySelector("#hire-me .close-btn");
+
+  closeBtn.onclick = () => {
+    const hireMeAlert = document.querySelector("#hire-me");
+    hireMeAlert.style.opacity = "0";
+
+    setTimeout(function() {
+      hireMeAlert.parentNode.removeChild(hireMeAlert);
+    }, 500);
+  };
+};
